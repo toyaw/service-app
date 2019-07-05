fda5b1d72f835e297a7afe03b2267f5ee581fc6c
Toya Williams <toyaw7@gmail.com>
2019-07-04 20:20:14 -0400

    setting up mongoose

--- models/connection.js
+++ models/connection.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:3000/UserDB', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+
+require('./users.model');
