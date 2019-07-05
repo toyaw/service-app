c108d187e3ec6ee408baf053309466e136a8bf2f
Toya Williams <toyaw7@gmail.com>
2019-07-04 20:33:19 -0400

    setting schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+
+userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'This field is required.'
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: String
+    },
+    city: {
+        type: String
+    }
+});
